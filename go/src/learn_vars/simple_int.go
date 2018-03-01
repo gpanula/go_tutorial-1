63d76992c109ec5b433a091d022193c3983beda8
trentonj <trentonj@kablah.com>
2018-02-28 19:20:19 -0600

    deleted int

--- go/src/learn_vars/simple_int.go
+++ go/src/learn_vars/simple_int.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	var width, height int = 100, 50 // declare two vars in a singal go
+	var width, height = 100, 50 // declare two vars in a singal go
 	fmt.Println("width is", width, "height is", height)
 }
