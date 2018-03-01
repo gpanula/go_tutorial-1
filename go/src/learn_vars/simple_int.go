34e92cb8bc974a4ed86182ad07c247999b067110
trentonj <trentonj@kablah.com>
2018-02-28 19:23:23 -0600

    more multi variables

--- go/src/learn_vars/simple_int.go
+++ go/src/learn_vars/simple_int.go
@@ -5,4 +5,8 @@ import "fmt"
 func main() {
 	var width, height = 100, 50 // declare two vars in a singal go
 	fmt.Println("width is", width, "height is", height)
+	// now assign values to width  and height 
+	width = 75
+	height = 200
+	fmt.Println ("width is", width, "height", height)
 }
