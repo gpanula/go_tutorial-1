872baf370d53bf9a654c0c1a8a429b4da668919e
trentonj <trentonj@kablah.com>
2018-02-28 19:10:22 -0600

    assign initial value of age to 75

--- go/src/learn_vars/simple_int.go
+++ go/src/learn_vars/simple_int.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var age int // declare age variable as integer
+	var age int = 75 // declare age with initial value of 75
 	fmt.Println("declared age int and its value is ", age)
 	age = 29 // assign value of 29 to age
 	fmt.Println("age now has value of" , age)
